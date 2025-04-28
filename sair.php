6ee21190002699986307f6c6437fde7d06f03094
Kaike Correia <148728784+oKaike@users.noreply.github.com>
2025-04-28 14:45:32 -0300

    Update sair.php

--- sair.php
+++ sair.php
@@ -1,8 +1,8 @@
-<?php
+a<?php
 if(!isset($_SESSION)){
-    session_start();
+    session_start();//Checa se a seção existe
 }
-session_destroy();
-header('Location:Tabela.php');
+session_destroy();//Enx=cerra a sesação
+header('Location:Tabela.php');//Leva o ususario para a seção inicial
 die();
 ?>
